81c056d7d39fd564829be5272eb0cf5bded9604e
Instil Trainers <training@instil.co>
2015-11-24 13:37:24 +0000

    added tests

--- javascript/crimes_app.js
+++ javascript/crimes_app.js
@@ -1,50 +1,54 @@
-
 (function () {
 
     'use strict';
 
     angular.module("crimesApp", [])
-           .service("CrimesService", ["$http", CrimesService])
-           .controller("CrimesController", ["CrimesService", CrimesController])
+           .value("crimesUrl", "http://data.police.uk/api/crimes-street/all-crime?lat=54.586173&lng=-5.932221&date=")
+           .value("defaultDate", new Date())
+           .factory("CrimesService", ["$http", "$q", "crimesUrl", CrimesService])
+           .controller("CrimesController", ["CrimesService", "$q", "defaultDate", CrimesController])
            .directive("crimeLocationLink", CrimeLocationLink);
 
 
-    function CrimesService($http) {
-        this.$http = $http;
+    function CrimesService($http, $q, crimesUrl) {
+        return {
+            fetchCrimes: function(date) {
+                var deferred = $q.defer();
+                $http.get(crimesUrl + date).then(function(response) {
+                    deferred.resolve(response.data);
+                });
+                return deferred.promise;
+            }
+        };
     }
 
-    CrimesService.prototype.fetchCrimes = function(date, callback) {
-        var url = "http://data.police.uk/api/crimes-street/all-crime?lat=54.586173&lng=-5.932221&date=" + date;
-        this.$http.get(url)
-             .success(function(data) {
-                 callback(data);
-             });
-    };
-
-    function CrimesController(CrimesService) {
+    function CrimesController(CrimesService, $q, defaultDate) {
         this.crimes = [];
         this.crimesService = CrimesService;
+        this.$q = $q;
         this.lastSearchedDate = "";
-        this.searchDate = new Date();
+        this.searchDate = defaultDate;
+        this.isWorking = false;
     }
 
     CrimesController.prototype.getCrimes = function() {
         var self = this;
+        var deferred = this.$q.defer();
         if (self.searchDate !== self.lastSearchedDate) {
             self.lastSearchedDate = self.searchDate;
-            var searchDateString = this.searchDate.getFullYear() + "-" + this.searchDate.getMonth();
-            self.crimesService.fetchCrimes(searchDateString, function(crimes){
+            var searchDateString = this.searchDate.getFullYear() + "-" + (this.searchDate.getMonth() + 1);
+            self.isWorking = true;
+            self.crimesService.fetchCrimes(searchDateString).then(function(crimes){
                 self.crimes = crimes;
+                self.isWorking = false;
+                deferred.resolve(self.crimes);
             });
         }
+        return deferred.promise;
     };
 
     CrimesController.prototype.categoryOf = function(crime) {
         return crime.category;
-    }
-
-    CrimesController.prototype.locationOf = function(crime) {
-        return crime.location.street.name;
     };
 
     CrimesController.prototype.outcomeOf = function(crime) {
@@ -54,9 +58,9 @@
     function CrimeLocationLink() {
         return {
             restrict: "E",
-            template: '<a ng-href="http://maps.google.co.uk?q=loc:{{lat}}+{{lng}}" target="_blank">{{label}}</a>',
+            transclude: true,
+            template: '<a ng-href="http://maps.google.co.uk?q=loc:{{lat}}+{{lng}}" target="_blank"><ng-transclude></ng-transclude></a>',
             scope: {
-                label: "=label",
                 lat: "=lat",
                 lng: "=lng"
             }
---
+++ spec/crimesSpec.js
@@ -0,0 +1,109 @@
+(function () {
+
+    'use strict';
+
+    describe("Crimes controller", function() {
+
+        var crimesController, $httpBackend, $rootScope;
+        var crimesUrl = "http://crime-data.uk?date=";
+        var searchDate = "2015-11";
+
+        var crimes = [
+            {
+                "category": "Crimes against code",
+                "location": {
+                    "latitude": "54.595793",
+                    "longitude": "-5.930977",
+                    "street": {
+                        "name": "The Office"
+                    }
+                },
+                "outcome_status": {
+                    "category": "ongoing investigation",
+                    "date": "2015-11"
+                }
+            }
+        ];
+
+        beforeEach(module("crimesApp", function($provide){
+            $provide.value("crimesUrl", crimesUrl);
+            $provide.value("defaultDate", new Date(searchDate));
+        }));
+
+        beforeEach(inject(function($controller, _$httpBackend_, _$rootScope_, CrimesService){
+            $httpBackend = _$httpBackend_;
+            $rootScope = _$rootScope_;
+            crimesController = $controller("CrimesController", {CrimesService:CrimesService});
+        }));
+
+        it("should fetch its data from a remote service", function() {
+            $httpBackend.expectGET(crimesUrl + searchDate).respond(function() {
+                return crimes;
+            });
+            crimesController.getCrimes();
+            $httpBackend.flush();
+        });
+
+        it("should not fetch remote data when the same search is being done", function() {
+            $httpBackend.expectGET(crimesUrl + searchDate).respond(function() {
+                return [200, crimes];
+            });
+            crimesController.getCrimes();
+            crimesController.getCrimes();
+            $httpBackend.flush();
+        });
+
+        it("should use the data returned from the remote service", function(done) {
+            $httpBackend.whenGET(crimesUrl + searchDate).respond(function() {
+                return [200, crimes];
+            });
+            crimesController.getCrimes().then(function(){
+                expect(crimesController.crimes).toEqual(crimes);
+                done();
+            });
+            $httpBackend.flush();
+        });
+
+        it("should return the category of a crime", function() {
+            var crime = {category: "crime"};
+            expect(crimesController.categoryOf(crime)).toEqual("crime");
+        });
+
+        it("should return the outcome of a crime", function() {
+            var crime = {outcome_status: {category: "outcome"}};
+            expect(crimesController.outcomeOf(crime)).toEqual("outcome");
+        });
+
+        it("should return N/A for the outcome of a crime when no outcome is available", function() {
+            var crime = {outcome_status: null};
+            expect(crimesController.outcomeOf(crime)).toEqual("N/A");
+        });
+
+        afterEach(function() {
+            $rootScope.$apply();
+        });
+
+    });
+
+    describe("CrimeLocationLink directive", function() {
+
+        var $compile;
+        var $rootScope;
+
+        beforeEach(module("crimesApp"));
+
+        beforeEach(inject(function(_$compile_, _$rootScope_) {
+            $compile = _$compile_;
+            $rootScope = _$rootScope_;
+        }));
+
+        it("should build an anchor tag using the provided details", function() {
+            var link = $compile("<crime-location-link lat='1' lng='2'>label</crime-location-link>")($rootScope);
+            $rootScope.$digest();
+            expect(link.text()).toEqual("label");
+            expect(link.html()).toContain('href="http://maps.google.co.uk?q=loc:1+2">');
+        });
+
+    });
+
+}());
